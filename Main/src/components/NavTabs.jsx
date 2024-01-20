27f6a5f4d9d810371bfd04fb6c581a289e1ceb0b
RickiBobbii <rickibobbiii@gmail.com>
2024-01-19 19:52:47 -0500

    fixed nav alignment

--- Main/src/components/NavTabs.jsx
+++ Main/src/components/NavTabs.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
@@ -6,46 +6,69 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    // added fixed-top
-    <ul className="nav nav-pills nav-justified fixed-top bg-dark">
-      <li className="nav-link">
-        <Link
-          to="/"
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+    // TEST Navbar
+    <nav className="navbar navbar-expand-xl navbar-dark bg-dark fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand fs-1" href="/">
+          Dusty Gray
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarDark"
+          aria-controls="navbarDark"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          Home
-        </Link>
-      </li>
-      <li className="nav-link">
-        <Link
-          to="/About"
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </Link>
-      </li>
-      <li className="nav-link">
-        <Link
-          to="/Portfolio"
-          // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </Link>
-      </li>
-      <li className="nav-link">
-        <Link
-          to="/Contact"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </Link>
-      </li>
-    </ul>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarDark">
+          <ul className="navbar-nav ms-auto mb-2 mb-xl-0">
+            <li className="nav-item fs-5 mx-2">
+              <Link
+                to="/"
+                // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+                // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+                className={currentPage === "/" ? "nav-link active" : "nav-link"}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item fs-5 mx-2">
+              <Link
+                to="/About"
+                className={
+                  currentPage === "/About" ? "nav-link active" : "nav-link"
+                }
+              >
+                About Me
+              </Link>
+            </li>
+            <li className="nav-item fs-5 mx-2">
+              <Link
+                to="/Portfolio"
+                className={
+                  currentPage === "/Portfolio" ? "nav-link active" : "nav-link"
+                }
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item fs-5 mx-2">
+              <Link
+                to="/Contact"
+                className={
+                  currentPage === "/Contact" ? "nav-link active" : "nav-link"
+                }
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
