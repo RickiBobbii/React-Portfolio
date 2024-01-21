94576ee16cde5a9077251a01bff015c7b4ef0fc5
RickiBobbii <rickibobbiii@gmail.com>
2024-01-21 16:14:07 -0500

    added about template and pic

--- Main/src/pages/About.jsx
+++ Main/src/pages/About.jsx
@@ -1,15 +1,66 @@
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+import profilePic from "../assets/profilePic.jpg";
+
 export default function About() {
+
+  const socialLinks = {
+    height: "2rem",
+    width: "2rem",
+  };
+
   return (
-    <div className="vh-100">
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+   
+
+    <div className="container vh-100 pt-5">
+      <div className="row text-center justify-content-center">
+        <div className="col col-md-8 mb-5">
+          <div className="rounded shadow-sm py-5 px-4 bg-white p-2 text-dark bg-opacity-50">
+            <img
+              src={profilePic}
+              alt=""
+              width="200"
+              className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+            ></img>
+            <h1 className="mb-3">Dusty Gray</h1>
+            <h2 className="fs-3 text-uppercase text-muted">
+              Full Stack Web Developer
+            </h2>
+            <h2>About Me</h2>
+            <p className="fs-5">
+              Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+              Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean
+              sed mollis diam. Nunc aliquet risus ac finibus porta. Nam quis
+              arcu non lectus tincidunt fermentum. Suspendisse aliquet orci
+              porta quam semper imperdiet. Praesent euismod mi justo, faucibus
+              scelerisque risus cursus in. Sed rhoncus mollis diam, sit amet
+              facilisis lectus blandit at.
+            </p>
+            <ul className="mb-0 list-inline mt-3">
+              <li className="list-inline-item">
+                <a href="#" className="">
+                  <i className="fs-5">
+                    <FaGithub />
+                  </i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#" className="">
+                  <i className="fs-5">
+                    <FaEnvelope />
+                  </i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#" className="socialLinks">
+                  <i className="fs-5">
+                    <FaLinkedin />
+                  </i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
